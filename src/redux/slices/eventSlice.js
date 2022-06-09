e130e687d357a2e14dba2fad84d36833ed5c110e
John Shin <shinhs0506@gmail.com>
2022-06-08 20:07:57 -0700

    initiated event redux

---
+++ src/redux/slices/eventSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const eventSlice = createSlice({
+    name: 'eventSlice',
+    initialState: [],
+    reducers: {
+        // reducer functions go here
+    }
+})
+
+// export reducer functions here
+// export const {} = slice.actions
+export default eventSlice.reducer
