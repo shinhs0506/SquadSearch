639a683bead51c509c50517a3c443b3d99bb0598
John Shin <shinhs0506@gmail.com>
2022-06-23 23:13:10 -0700

    searchbar filter

--- frontend/src/components/eventCard/eventCardContainer.jsx
+++ frontend/src/components/eventCard/eventCardContainer.jsx
@@ -4,25 +4,18 @@ import EventCard from 'components/eventCard/eventCard';
 import './eventCardContainer.css';
 
 export default function EventCardContainer() {
-    const curr = useSelector((state) => state.event.events);
-    const elements = curr.map((event) => (
-        // TODO: change key value to id of events
-        <EventCard
-          key={event.Name + event.Date}
-          name={event.Name}
-          date={event.Date}
-          location={event.Location}
-        />
-    ));
+    const events = useSelector((state) => state.event.events);
+
     return (
         <div className="container">
-            <EventCard
-              img="https://i.scdn.co/image/ab6761610000e5ebcdce7620dc940db079bf4952"
-              name="Ariana Grande"
-              date="2022-06-23"
-              location="Rogers Arena"
-            />
-            {elements}
+            { events.map((event) => (
+                <EventCard
+                  key={event.name + event.date}
+                  name={event.name}
+                  date={event.date}
+                  location={event.location}
+                />
+            ))}
         </div>
     );
 }
