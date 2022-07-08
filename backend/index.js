940419abb0ec6b1c0e65a47dc612d4f820cca49b
John Shin <shinhs0506@gmail.com>
2022-07-07 23:50:54 -0700

    backend auth api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import passport from 'passport';
+
 import router from './router.js';
+import applyPassportStrategy from './passport.js';
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// mongodb
+mongoose.connect('mongodb://localhost/cpsc455-SquadSearch', {});
+const { connection } = mongoose;
+connection.once('open', () => {
+    // console.log('connected to mongo database')
+});
+
+// passport middleware
+applyPassportStrategy(passport);
+
+// routes
 app.use('/', router);
 
 const port = 4000;
