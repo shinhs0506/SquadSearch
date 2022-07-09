c9ebc48eb5c5bac2ed72650d2910e74112660135
John Shin <shinhs0506@gmail.com>
2022-07-09 13:26:11 -0700

    set up auth guard

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// TODO: Maybe have this be a pop-up instead
+
+function Login() {
+    return (
+        <>
+            <div>
+                Login
+            </div>
+            <div>
+                Login
+            </div>
+            <div>
+                Login
+            </div>
+            <div>
+                Login
+            </div>
+        </>
+    );
+}
+
+export default Login;
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate, useLocation } from 'react-router-dom';
 import Home from 'pages/home/home';
 import Message from 'pages/message/message';
+import Login from 'pages/login/login';
 import SignUp from 'pages/signUp/signUp';
 import CreateEvent from 'pages/createEvent/createEvent';
 import Chatboard from 'pages/chatboard/chatboard';
 import EditProfile from 'pages/editProfile/editProfile';
+import { Toolbar } from '@mui/material';
+
+function Guard(props) {
+    const { routeRedirect } = props
+    const location = useLocation();
+
+    return false
+        ? <Outlet />
+        : <Navigate to={routeRedirect} replace state={{ from: location }} />
+}
 
 function AppRoutes() {
     return (
+        <>
+        <Toolbar />
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/message" element={<Message />} />
-            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/chatboard" element={<Chatboard />} />
-            <Route path="/edit-profile" element={<EditProfile />} />
-        </Routes>
+
+            <Route element={<Guard routeRedirect="/login" />} >
+                <Route path="/" element={<Home />} />
+                <Route path="/message" element={<Message />} />
+                <Route path="/create-event" element={<CreateEvent />} />
+                <Route path="/chatboard" element={<Chatboard />} />
+                <Route path="/edit-profile" element={<EditProfile />} />
+            </ Route>
+        </ Routes>
+        </>
     );
 }
 
