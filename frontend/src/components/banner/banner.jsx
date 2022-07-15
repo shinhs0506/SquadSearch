8ab80a1e6fe868f9f4b9bc945e41ac3efeb15d84
John Shin <shinhs0506@gmail.com>
2022-07-15 12:04:05 -0700

    fixes #30

--- frontend/src/components/banner/banner.jsx
+++ frontend/src/components/banner/banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Snackbar from '@mui/material/Snackbar';
+
 import { authSliceActions } from 'redux/slices/authSlice';
 import { eventSliceActions } from 'redux/slices/eventSlice';
 
