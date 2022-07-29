bad57e9ff3099b6e0e4be90a8f049f7945edbdc3
baekanthony <53468745+baekanthony@users.noreply.github.com>
2022-07-28 23:27:03 -0700

    merge conflicts

--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import eventController from './controllers/eventController.js';
 import authController from './controllers/authController.js';
+import messageController from './controllers/messageController.js';
 
 const router = express.Router();
 
@@ -13,5 +14,7 @@ router.post('/api/auth/logout', authController.logoutUser);
 router.get('/api/events', eventController.getAllEvents);
 router.post('/api/events', eventController.createEvent);
 router.delete('/api/event/:id', eventController.deleteEventByID);
+
+router.get('/api/messages', messageController.getAllChats);
 
 export default router;
